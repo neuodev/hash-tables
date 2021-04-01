fc4988b9cd9323f2a5f85862c55c277a6c451588
Ahmed Ibrahim <ib153507@gmail.com>
2021-04-01 08:29:58 +0200

    Add Set insted of normal objects

--- First_Repeated_Character.ts
+++ First_Repeated_Character.ts
@@ -2,12 +2,12 @@ function fristRepeated(str: string) {
   if (!str) return false;
   if (str.length === 1) return str;
   str = str.toLowerCase();
-  let hashTable = {};
+  let set = new Set();
   for (let i = 0; i < str.length; i++) {
     let char = str[i];
     if (char !== ' ') {
-      if (hashTable[char]) return char;
-      hashTable[char] = 1;
+      if (set.has(char)) return char;
+      set.add(char);
     }
   }
 
